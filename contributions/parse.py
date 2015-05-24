f2d2f13f53c36b7f3c752b98c88d408687c64c48
Alex Chan <alex@alexwlchan.net>
2015-05-24 14:00:52 +0100

    Added functions for calculation streak lengths

--- contributions/parse.py
+++ contributions/parse.py
@@ -1,43 +1,109 @@
 #!/usr/bin/env python
 
+from collections import defaultdict
 import datetime
+
 
 def within_last_year(date):
     """
     Returns True if a date falls within the last year, False otherwise.
 
-    Takes a datetime object as an argument.
+    Takes a datetime.date as an argument.
     """
     today = datetime.date.today()
-    return datetime.datetime(today.year - 1, today.month, today.day) < date
+    return datetime.date(today.year - 1, today.month, today.day) < date
 
-def parse_contributions(file_path, date_format="%Y-%m-%d", separator=" "):
+
+def parse_contributions(file_path):
     """
     Given a path to a file, this function parses the file. It expects each line
     of the file to be in the format
 
-        (date_fmt)(separator)(value)
-
-    so an example line with the default settings would be
-
-        2015-05-24 22
+        %Y-%m-%d $value
 
     Returns a dict in which the keys are the dates.
     """
-    results = {}
+    results = defaultdict(int)
 
     with open(file_path) as ff:
         for line in ff:
-            date_str, value = line.strip().split(separator)
+
+            # Skip empty lines
+            if not line.strip():
+                continue
+
+            date_str, value = line.strip().split(" ")
 
             try:
-                date = datetime.datetime.strptime(date_str, date_format)
+                date = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
             except ValueError:
                 print "Malformed date string in line:\n{}".format(line.strip())
                 raise
 
             # Only add the result if the date is within the last year
             if within_last_year(date):
-                results[date] = int(value)
+                results[date] += int(value)
 
     return results
+
+
+def total_contributions(results):
+    """
+    Given a results dictionary, this returns the total number of contributions
+    in the last year.
+    """
+    return sum(results.itervalues())
+
+
+def longest_streak(results):
+    """
+    Returns the longest streak in the results. Assumes that results is
+    non-empty.
+    """
+    # First, we turn the list of dates into integers counting the number of
+    # days since 1 year ago.
+    today = datetime.date.today()
+    start = datetime.date(today.year - 1, today.month, today.day)
+
+    days = sorted((day - start).days for day in results.keys()
+                                         if results[day] > 0)
+
+    # To find the longest streak, we split the list 'days' into runs of
+    # consecutive integers.
+    streaks = []
+    current_streak = [days[0]]
+
+    # Go through each day in the list. If it's one after the last day in
+    # 'current', then append it to that streak and carry on. If not, save the
+    # current streak and start a new one.
+    for idx in range(1, len(days)):
+        if days[idx] == current_streak[-1] + 1:
+            current_streak.append(days[idx])
+        else:
+            streaks.append(current_streak)
+            current_streak = [days[idx]]
+
+    # When we've run out of days, append the last streak.
+    streaks.append(current_streak)
+
+    # Get the longest streak. Because the list is sorted in ascending order,
+    # if there are multiple longest streaks of the same length, it gets the
+    # first such streak.
+    longest_streak = max(streaks, key=len)
+
+    # Turn the list back into a series of dates
+    return [start + datetime.timedelta(day) for day in longest_streak]
+
+
+def current_streak(results):
+    """
+    Returns the length of the current streak in the results.
+    """
+    today = start = datetime.date.today()
+    streak_length = 0
+
+    while start in results.keys():
+        start -= datetime.timedelta(1)
+        streak_length += 1
+
+    return streak_length
